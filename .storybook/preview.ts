7b1e05d32eabd4734ece2f5fded6acbbbeb4cc6f
Victor S. Seferidis <seferidis@outlook.com>
2025-03-29 09:44:17 +0100

    added new attributes to button and input

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import '../src/styles/colors.css';
+import '../src/styles/fonts.css';
 import '../src/components/Button/Button.css';
 
 const preview: Preview = {
